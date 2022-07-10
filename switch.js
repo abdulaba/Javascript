909c4d6d4b036c4c0df3ed9f282f7ba265fc83ca
andres ibanez <andresibanezulloa@gmail.com>
2022-07-10 18:56:32 -0400

    pruebas

---
+++ switch.js
@@ -0,0 +1,11 @@
+let numero = 1
+switch(numero){
+  case 1:
+        console.log("Soy un uno");
+        break;
+  case 2:
+        console.log("Soy un dos");
+        break;
+  default:
+        console.log("No soy nada");
+}
